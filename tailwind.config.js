4c514a5e7a4e17ff47a4e13f62333cb019180531
Franco Aguirre <fn.aguirre@hotmail.com>
2023-11-02 15:49:27 -0300

    agrego colores y demas

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,8 @@ export default {
         colors:{
             primary: '#0B0D17',
             secondary: '#D0D6f9',
-            thirt: '#FFFFFF'
+            thirt: '#FFFFFF',
+            four:'#4D5057'
 
         },
         fontFamily: {
@@ -16,16 +17,29 @@ export default {
             secondary: 'Barlow Condensed'
         },
         backgroundImage: {
-            'bgMobile': 'url(\'src/assets/home/background-home-mobile.jpg\')',
-            'bgTablet': 'url(\'src/assets/home/background-home-tablet.jpg\')',
-            'bgDesktop': 'url(\'src/assets/home/background-home-desktop.jpg\')',
+            'bgMobileHome': 'url(\'src/assets/home/background-home-mobile.jpg\')',
+            'bgTabletHome': 'url(\'src/assets/home/background-home-tablet.jpg\')',
+            'bgDesktopHome': 'url(\'src/assets/home/background-home-desktop.jpg\')',
+
+            'bgMobileDestination': 'url(\'src/assets/destination/background-destination-mobile.jpg\')',
+            'bgTabletDestination': 'url(\'src/assets/destination/background-destination-tablet.jpg\')',
+            'bgDesktopDestination': 'url(\'src/assets/destination/background-destination-desktop.jpg\')',
+
+            'bgMobileCrew': 'url(\'src/assets/crew/background-crew-mobile.jpg\')',
+            'bgTabletCrew': 'url(\'src/assets/crew/background-crew-tablet.jpg\')',
+            'bgDesktopCrew': 'url(\'src/assets/crew/background-crew-desktop.jpg\')',
+
+            'bgMobileTechnology': 'url(\'src/assets/Technology/background-Technology-mobile.jpg\')',
+            'bgTabletTechnology': 'url(\'src/assets/Technology/background-Technology-tablet.jpg\')',
+            'bgDesktopTechnology': 'url(\'src/assets/Technology/background-Technology-desktop.jpg\')',
             
          
         },
         backgroundColor:{
             'bgGradient': 'rgba(208,204,209, 0.45)',
             'bgGradientMobile': 'rgba(10,20,30, 1)',
-            'bg-button':'#FFFFFF'
+            'bg-button':'#FFFFFF',
+            'bg-transparent':'rgba(0,0,0, 0)'
         },
         screens: {
             sm: '480px',
